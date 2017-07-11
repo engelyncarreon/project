7dfb15c6c4c9deef2e7950f135241310104e7da9
marinelbaucas <marinelbaucas@gmail.com>
2017-07-12 00:42:04 +0800

    update lesson area

--- nelenge/student/pages/LessonArea.php
+++ nelenge/student/pages/LessonArea.php
@@ -180,42 +180,112 @@
                         if(empty($crow['url'])) { 
 
                     ?>
+                        
                         <p>
 
-                            <?php echo $crow['lecture_details']; ?>
-                                
-                        </p>
+                            <?php echo $crow['lecture_details']; 
+
+                    ?>  </p>
 
                     <?php 
 
-                    }else { 
+                        }else { 
 
                         $url = $crow['url'];
-                        if($crow['filetype']=='Video'){ 
+                        if($crow['filetype']=='Video')
 
-                    ?>                                
-                            
-                        <video width='700' height="400" controls autoplay> 
-                            <source src="<?php echo $url ?>" type='video/mp4'></source>
-                        </video>      
+                        { 
 
+                    ?>             
+
+                        <video width='700' height="400" controls autoplay> <source src="<?php echo $url ?>" type='video/mp4'></source></video>               
                     <?php 
 
-                        }  else if($crow['filetype']=='image'){
-                            ?><object data="<?php echo $url ?>" width='700' height='400' ></object><?php
-                        }  else{ ?>
-                            <a href="#"><?php echo $crow['urlname']; ?></a>
+                            }  else if($crow['filetype']=='image'){
+                                ?><object data="<?php echo $url ?>" width='700' height='400' ></object>
 
                     <?php
 
-                        }
+                            }  else{ 
 
                     ?>
+
+                        <a href="#"><?php echo $crow['urlname']; ?></a>
+
+                    <?php
+                            }
+
+                    ?>
+
                         <p><?php echo $crow['lecture_details']; ?></p>
 
-                    <?php 
+                       <?php 
 
                         }
+
+                        $net = $crow['lecture_id']+1;
+                        $netless = $crow['lesson_id'];
+                        $netid = $crow['course_id'];
+                        $next = "Select * from lessons natural join lecture where lesson_id = '$netless' and lecture_id= '$net' and course_id = $netid";
+                       $nextres = mysqli_query($con, $next);
+                       $nextrow = mysqli_fetch_assoc($nextres);
+
+                       if(isset($nextrow)){
+                        ?>
+
+                        <a href="LessonArea.php?c=<?php echo $cid; ?>&d=<?php echo $nextrow['lesson_seq'];?>&m=<?php echo $nextrow['lecture_id'];?>&l=<?php echo $nextrow['lesson_id'];?>">
+
+                            <button >Next Lecture</button>
+
+                        </a>
+
+                        <?php
+
+                            } else {
+
+                            $net1 = $crow['lecture_id']+1;
+
+                            $netless1 = $crow['lesson_id']+1;
+
+                            $next1 = "Select * from lessons natural join lecture where lesson_id = '$netless1' and lecture_id= '$net1' and course_id = '$netid'";
+
+                           $nextres1 = mysqli_query($con, $next1);
+                           $nextrow1 = mysqli_fetch_assoc($nextres1);
+
+                           if(isset($nextrow1)){
+
+                                ?>
+
+                                <a href="LessonArea.php?c=<?php echo $cid; ?>&d=<?php echo $nextrow1['lesson_seq'];?>&m=<?php echo $nextrow1['lecture_id'];?>&l=<?php echo $nextrow1['lesson_id'];?>">
+                                
+                                <button >Next Lecture</button>
+
+                                </a>
+
+                                <?php
+                               }
+
+                            else{   
+
+                                echo $studrow1['student_id'];
+                                echo $studrow1['course_id'];
+
+                            ?> 
+
+                            <form action = "finishedcode.php" method="POST">
+
+                                <input type="hidden" value=<?php echo $studrow1['student_id']; ?> name ="stid" >
+                                <input type="hidden" value=<?php echo $studrow1['course_id']; ?> name="coid" >
+                                <input type="hidden" value=<?php echo $studrow1['lesson_id']; ?>  name = "leid">
+                                <input type="submit" value="Finished" name="finished">
+
+                            </form>
+
+                                <?php 
+                            }
+
+                       }
+
 
                     ?>
 
